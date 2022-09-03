cbc413293cb75b811d23c94327cfa0d7a6262f9f
lqsu <64267208+lqsu@users.noreply.github.com>
2022-09-03 18:06:00 +0800

    Update AutoSignMachine.js

--- AutoSignMachine.js
+++ AutoSignMachine.js
@@ -1,3 +1,12 @@
+enable_unicom=true 
+user=15694867110
+password=199971
+appid=247b001385de5cc6ce11731ba1b1583580b8159e5a51c4abd601e8fc1f8b2b87acf2234bc8a8487cb751a33da268d93d6c111416b0572de9880fc4ae86370754c30c206b71c709096316550b519b8394
+
+
+
+
+
 const yargs = require('yargs/yargs')
 // const { hideBin } = require('yargs/helpers')
 /**
